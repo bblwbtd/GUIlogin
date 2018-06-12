33f311430ba9f8cf34087495e30ad910a27b9ad9
lockoct <825300455@qq.com>
2018-06-12 21:21:43 +0800

    功能增加
    1.在命令类中的重置密码指令部分代码加入了玩家权限判断，不是op则不能使用
    2.控制台现已可以使用重置密码指令

--- src/group/ldgame/command/PluginCommand.java
+++ src/group/ldgame/command/PluginCommand.java
@@ -19,8 +19,8 @@ public class PluginCommand implements CommandExecutor {
 	@Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("guilogin")) {
-        	Player player = ((Player) sender).getPlayer();
             if (sender instanceof Player) {
+            	Player player = ((Player) sender).getPlayer();
             	//在游戏中输入指令
             	if(args.length == 0) {
             		sender.sendMessage("/guilogin reset <玩家名> - 重置玩家的密码(仅限op使用)");
@@ -29,30 +29,7 @@ public class PluginCommand implements CommandExecutor {
             	}
             	
             	if(args[0].equalsIgnoreCase("reset")) {
-            		if(args.length == 1) {
-            			//没有输入玩家名
-            			sender.sendMessage("请输入你要重置密码的玩家名");            			
-            		} else if(args.length == 2) {
-            			TreeMap<String, String> playerInfo = LoginInfoUtil.getPlayerLoginInfo(args[1]);
-            			
-            			//输入了玩家名
-            			if(playerInfo.get("playerexist").equals("yes")) {
-            				//配置文件中有该玩家信息
-            				if(!playerInfo.get("pwEncrypt").equals("")) {
-            					//配置文件中玩家加密信息不为空
-            					LoginInfoUtil.registerPlayerInfo(args[1], "");
-            					sender.sendMessage("玩家"+args[1]+"的密码已重置");            			
-            				} else {
-            					//配置文件中玩家加密信息为空(密码被重置过)
-            					sender.sendMessage("玩家"+args[1]+"的密码为空，无需重置");            			
-            				}
-            			} else {
-            				//配置文件中找不到该玩家
-            				sender.sendMessage("找不到该玩家");            			
-            			}
-            		} else {
-            			sender.sendMessage("请输入正确的重置密码指令");
-            		}
+            		return resetPassword(player, sender, args);
             	}
             	
             	if(args[0].equalsIgnoreCase("reg")) {
@@ -76,9 +53,79 @@ public class PluginCommand implements CommandExecutor {
             	}
             }else {
             	//在游戏外输入指令
-            	return true;
+            	if(args.length == 0) {
+            		sender.sendMessage("guilogin reset <玩家名> - 重置玩家的密码");
+            		return true;
+            	}
+            	
+            	if(args[0].equalsIgnoreCase("reset")) {
+            		return resetPassword(sender, args);
+            	}
             }
         }
         return true;
     }
+	
+	private boolean resetPassword(CommandSender sender,String[] args) {
+		if(args.length == 1) {
+			//没有输入玩家名
+			if(sender instanceof Player) {
+				sender.sendMessage("请输入你要重置密码的玩家名");				
+			}else {
+				System.out.println("请输入你要重置密码的玩家名");
+			}
+			return true;
+		} else if(args.length == 2) {
+			TreeMap<String, String> playerInfo = LoginInfoUtil.getPlayerLoginInfo(args[1]);
+			
+			//输入了玩家名
+			if(playerInfo.get("playerexist").equals("yes")) {
+				//配置文件中有该玩家信息
+				if(!playerInfo.get("pwEncrypt").equals("")) {
+					//配置文件中玩家加密信息不为空
+					LoginInfoUtil.registerPlayerInfo(args[1], "");
+					if (sender instanceof Player) {
+						sender.sendMessage("玩家"+args[1]+"的密码已重置"); 
+					} else {
+						System.out.println("玩家"+args[1]+"的密码已重置");
+					}
+					return true;
+				} else {
+					//配置文件中玩家加密信息为空(密码被重置过)
+					if (sender instanceof Player) {
+						sender.sendMessage("玩家"+args[1]+"的密码为空，无需重置"); 
+					} else {
+						System.out.println("玩家"+args[1]+"的密码为空，无需重置");
+					}
+					return true;
+				}
+			} else {
+				//配置文件中找不到该玩家
+				if (sender instanceof Player) {
+					sender.sendMessage("找不到该玩家的登录密码信息");
+				} else {
+					System.out.println("找不到该玩家的登录密码信息");
+				}
+				return true;
+			}
+		} else {
+			if (sender instanceof Player) {
+				sender.sendMessage("请输入正确的重置密码指令");
+			} else {
+				System.out.println("请输入正确的重置密码指令");
+			}
+			return true;
+		}
+	}
+	
+	private boolean resetPassword(Player player,CommandSender sender,String[] args) {
+		if(!player.isOp()) {
+			//玩家不是op,则不允许用该指令
+			sender.sendMessage("你没有权限使用该指令");
+			return true;
+		}else {
+			return resetPassword(sender,args);
+		}
+	}
+	
 }
